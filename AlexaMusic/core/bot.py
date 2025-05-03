1a85ebc213e97bad2e8b67a5f1b27cf31c85f455
zerosugar6464 <musickumal@gmail.com>
2025-05-03 23:33:10 +0300

    Update bot.py

--- AlexaMusic/core/bot.py
+++ AlexaMusic/core/bot.py
@@ -39,7 +39,7 @@ class AlexaBot(Client):
         self.mention = get_me.mention
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, "» ᴍᴜsɪᴄ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ, ᴡᴀɪᴛɪɴɢ ғᴏʀ ᴀssɪsᴛᴀɴᴛ..."
+                config.LOG_GROUP_ID, "» asistan aktif ✅ ."
             )
         except:
             LOGGER(__name__).error(
