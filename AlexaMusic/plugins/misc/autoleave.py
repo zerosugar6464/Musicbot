bfda6247e924d4248212a6765a65fa3b26b29ec5
zerosugar6464 <musickumal@gmail.com>
2025-05-03 21:20:27 +0300

    Update autoleave.py

--- AlexaMusic/plugins/misc/autoleave.py
+++ AlexaMusic/plugins/misc/autoleave.py
@@ -1,53 +1,45 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
 import asyncio
-from datetime import datetime
 from pyrogram.enums import ChatType
+from datetime import datetime, timedelta
 
 import config
 from AlexaMusic import app
-from AlexaMusic.core.call import Alexa, autoend
+from AlexaMusic.core.call import Alexa
 from AlexaMusic.utils.database import (
     get_client,
     is_active_chat,
     is_autoend,
+    get_assistant,
 )
 
 autoend = {}
 
-
 async def auto_leave():
-    if config.AUTO_LEAVING_ASSISTANT == str(True):
-        while not await asyncio.sleep(config.AUTO_LEAVE_ASSISTANT_TIME):
+    if config.AUTO_LEAVING_ASSISTANT:
+        while not await asyncio.sleep(900):
             from AlexaMusic.core.userbot import assistants
 
             for num in assistants:
                 client = await get_client(num)
+                left = 0
                 try:
                     async for i in client.get_dialogs():
-                        chat_type = i.chat.type
-                        if chat_type in [
+                        if i.chat.type in [
                             ChatType.SUPERGROUP,
                             ChatType.GROUP,
                             ChatType.CHANNEL,
                         ]:
-                            chat_id = i.chat.id
                             if (
-                                chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001686672798
+                                i.chat.id != config.LOGGER_ID
+                                and i.chat.id != -1001686672798
+                                and i.chat.id != -1001549206010
                             ):
-                                if not await is_active_chat(chat_id):
+                                if left == 20:
+                                    continue
+                                if not await is_active_chat(i.chat.id):
                                     try:
-                                        await client.leave_chat(chat_id)
+                                        await client.leave_chat(i.chat.id)
+                                        left += 1
                                     except:
                                         continue
                 except:
@@ -58,8 +50,9 @@ asyncio.create_task(auto_leave())
 
 
 async def auto_end():
-    while not await asyncio.sleep(30):
-        if not await is_autoend():
+    while not await asyncio.sleep(5):
+        ender = await is_autoend()
+        if not ender:
             continue
         for chat_id in autoend:
             timer = autoend.get(chat_id)
@@ -71,7 +64,7 @@ async def auto_end():
                     continue
                 autoend[chat_id] = {}
                 try:
-                    await Alexa.stop_stream(chat_id)
+                    await Anony.stop_stream(chat_id)
                 except:
                     continue
                 try:
