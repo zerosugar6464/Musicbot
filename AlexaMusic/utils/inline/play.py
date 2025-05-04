864e6f2d836b56daf65bb5021d4544ac33fa2f85
zerosugar6464 <musickumal@gmail.com>
2025-05-04 17:17:13 +0300

    Update play.py

--- AlexaMusic/utils/inline/play.py
+++ AlexaMusic/utils/inline/play.py
@@ -48,8 +48,15 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
             )
         ],
         [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
             InlineKeyboardButton(
                 text=_["PL_B_2"],
+           )
+         ],
+         [
                 callback_data=f"add_playlist {videoid}",
             ),
             InlineKeyboardButton(
